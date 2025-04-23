f4d800ccf8adf188bd15e274cf49ef106454452e
niisirana <niisirana@gmail.com>
2025-04-23 20:27:54 +0700

    final i think

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//burger-menu
+
 const toggleBtn = document.querySelector(".toggle_btn");
 const toggleBtnIcon = document.querySelector(".toggle_btn i");
 const dropDownMenu = document.querySelector(".dropdown-menu");
@@ -7,6 +9,12 @@ toggleBtn.onclick = function () {
   const isOpen = dropDownMenu.classList.contains("open");
 
   toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+
+  document.querySelectorAll(".nav-item").forEach((n) =>
+    n.addEventListener("click", () => {
+      dropDownMenu.classList.remove("open");
+    })
+  );
 };
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -14,8 +22,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const reviews = document.querySelectorAll(".review");
   const prevBtn = document.querySelector(".arrow-left");
   const nextBtn = document.querySelector(".arrow-right");
+
   let currentIndex = 0;
 
+  //carousel function
   function updateCarousel() {
     const slideWidth = reviews[0].offsetWidth;
     const gap = 3.75 * 16;
@@ -30,6 +40,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     prevBtn.disabled = currentIndex === 0;
     nextBtn.disabled = currentIndex === reviews.length - 1;
+
+    updateDots();
   }
 
   prevBtn.addEventListener("click", function () {
@@ -43,11 +55,38 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   updateCarousel();
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-  var phoneInput = document.getElementById("phone");
-  var maskOptions = {
+  let startX = 0;
+
+  container.addEventListener("touchstart", (e) => {
+    startX = e.touches[0].clientX;
+  });
+
+  container.addEventListener("touchend", (e) => {
+    const endX = e.changedTouches[0].clientX;
+    const deltaX = endX - startX;
+
+    if (deltaX < -50) {
+      currentIndex = (currentIndex + 1) % reviews.length;
+      updateCarousel();
+    }
+
+    if (deltaX > 50) {
+      currentIndex = (currentIndex - 1 + reviews.length) % reviews.length;
+      updateCarousel();
+    }
+  });
+
+  //dots
+  function updateDots() {
+    const dots = document.querySelectorAll(".dot");
+    dots.forEach((dot) => dot.classList.remove("active"));
+    dots[currentIndex].classList.add("active");
+  }
+
+  //phone mask
+  const phoneInput = document.getElementById("phone");
+  const maskOptions = {
     mask: "+{7} (000) 000-00-00",
   };
 
